1a068fb42d3ce6a0178a279d141930280eeadad4
yonakisaragi <yonaadehermawan@gmail.com>
2020-10-08 23:59:09 +0700

    Proyek Ke-3

--- app/src/main/java/com/example/proyek3/InsertAndViewActivity.java
+++ app/src/main/java/com/example/proyek3/InsertAndViewActivity.java
@@ -1,9 +1,14 @@
 package com.example.proyek3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
